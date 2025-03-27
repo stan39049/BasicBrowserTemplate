3bf4c1482d21ab6de0ecfc70b704716b4d43081f
stan39049 <stan39049@gmail.com>
2025-03-27 12:00:54 -0400

    Added network permissions and loaded webview on button clicked and adds https// to url if it is not present

--- app/src/main/java/edu/temple/basicbrowser/MainActivity.kt
+++ app/src/main/java/edu/temple/basicbrowser/MainActivity.kt
@@ -21,11 +21,24 @@ class MainActivity : AppCompatActivity() {
         goButton = findViewById(R.id.goButton)
         webView = findViewById(R.id.webView)
 
+        webView.settings.javaScriptEnabled = true
+
         // Allow your browser to intercept hyperlink clicks
         webView.webViewClient = object: WebViewClient() {
             override fun onPageFinished(view: WebView?, url: String?) {
+                urlEditText.setText(url)
                 super.onPageFinished(view, url)
             }
+        }
+
+        goButton.setOnClickListener{
+            var url = urlEditText.text.toString()
+            if(url.startsWith("http")){
+                webView.loadUrl(url)
+            }else{
+                webView.loadUrl("https://" + url)
+            }
+
         }
 
     }
